bc0c5a3c681a8e6ba7129aeb13b97f6d80a05ee9
sumit <sumitbanty2004@gmail.com>
2026-07-31 00:35:28 +0530

    Project ready for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
-  console.log("TOKEN FROM LOCAL STORAGE:", token);
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
