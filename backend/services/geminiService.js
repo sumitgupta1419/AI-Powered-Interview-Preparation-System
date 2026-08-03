dd6b8ca9ff3b2296f6e2901b240f56717755ee17
sumit <sumitbanty2004@gmail.com>
2026-08-03 18:22:43 +0530

    Updated Gemini model

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -23,7 +23,7 @@ async function generateInterviewQuestions(
 ) {
   try {
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.5-flash",
+      model: "gemini-2.5-flash-lite",
     });
 
     const prompt = `
